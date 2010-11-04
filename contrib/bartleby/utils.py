d06cc33fe4a5c607ca99bd27f047ac0bfd4554fb
Stephen Burrows <stephen.r.burrows@gmail.com>
2010-11-04 14:57:03 -0400

    Improved efficiency and ability to function of get_entity_forms/get_view_forms utility functions.

--- contrib/bartleby/utils.py
+++ contrib/bartleby/utils.py
@@ -4,45 +4,55 @@ from philo.contrib.bartleby.models import Form
 from philo.models import Attribute, Entity
 
 
-def get_entity_forms(entity, passthrough=True):
+QUERYSET = 0
+SET = 1
+
+
+def get_entity_forms(entity, passthrough=True, return_value=QUERYSET):
+	if return_value not in [QUERYSET, SET]:
+		raise ValueError("Invalid return value type: %s" % return_value)
+	
 	if not isinstance(entity, Entity):
 		raise TypeError
 	
 	if passthrough and entity.attributes.passthrough is not None:
 		exclude_keys = [] # Exclude keys already found
+		attribute_pks = set()
 		
-		def get_attribute_forms(qs_mapper):
-			qs = Form.objects.none()
-			
-			for attribute in qs_mapper.queryset.exclude(key__in=exclude_keys):
-				fk_q = Q(foreignkeyvalue_set__attribute_set=attribute)
-				m2m_q = Q(foreignkeyvalue_set__manytomanyvalue__attribute_set=attribute)
-				
-				qs |= Form.objects.filter(fk_q | m2m_q)
-				exclude_keys.append(attribute.key)
+		def populate_attribute_pks(qs_mapper):
+			for pk, key in qs_mapper.queryset.exclude(key__in=exclude_keys).values_list('pk', 'key'):
+				attribute_pks.add(pk)
+				exclude_keys.append(key)
 			
 			if qs_mapper.passthrough:
-				qs |= get_attribute_forms(qs_mapper.passthrough)
-			
-			return qs
+				populate_attribute_pks(qs_mapper.passthrough)
 		
-		return get_attribute_forms(entity.attributes).distinct()
+		populate_attribute_pks(entity.attributes)
+		fk_q = Q(foreignkeyvalue_set__attribute_set__pk__in=attribute_pks)
+		m2m_q = Q(foreignkeyvalue_set__manytomanyvalue__attribute_set__pk__in=attribute_pks)
+		form_pks = set(Form.objects.filter(fk_q | m2m_q).values_list('pk', flat=True).distinct())
+		
+		if return_value == QUERYSET:
+			return Form.objects.filter(pk__in=form_pks)
+		else:
+			return form_pks
 	
 	entity_ct = ContentType.objects.get_for_model(entity)
 	
-	fk_q = Q(foreignkeyvalue_set__attribute_set__entity_content_type=entity_ct) &\
-		Q(foreignkeyvalue_set__attribute_set__entity_object_id=entity.pk)
-	m2m_q = Q(foreignkeyvalue_set__manytomanyvalue__attribute_set__entity_content_type=entity_ct) &\
-		Q(foreignkeyvalue_set__manytomanyvalue__attribute_set__entity_object_id=entity.pk)
+	fk_q = Q(foreignkeyvalue_set__attribute_set__entity_content_type=entity_ct) & Q(foreignkeyvalue_set__attribute_set__entity_object_id=entity.pk)
+	m2m_q = Q(foreignkeyvalue_set__manytomanyvalue__attribute_set__entity_content_type=entity_ct) & Q(foreignkeyvalue_set__manytomanyvalue__attribute_set__entity_object_id=entity.pk)
 	
-	return Form.objects.filter(fk_q | m2m_q).distinct()
+	if return_value == QUERYSET:
+		return Form.objects.filter(fk_q | m2m_q).distinct()
+	else:
+		return set(Form.objects.filter(fk_q | m2m_q).values_list('pk', flat=True))
 
 
 def get_view_forms(view, node):
 	# Until http://code.djangoproject.com/ticket/14609 is resolved, the following line will not work.
 	#return get_entity_forms(view, passthrough=False) | get_entity_forms(node)
-	# WORKAROUND
-	view_pks = [form.pk for form in get_entity_forms(view, passthrough=False)]
-	node_pks = [form.pk for form in get_entity_forms(node)]
-	return Form.objects.filter(pk__in=view_pks + node_pks)
+	# WORKAROUND - may actually be more efficient.
+	form_pks = get_entity_forms(view, passthrough=False, return_value=SET)
+	form_pks |= get_entity_forms(node, return_value=SET)
+	return Form.objects.filter(pk__in=form_pks)
 	# END WORKAROUND
