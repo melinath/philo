87b1d0c3d971b2c63b1d8c7790eae7117165f9ce
Harris Lapiroff <harris@chromamine.com>
2011-07-22 16:09:53 -0400

    Added javscript that detaches extra fields from the attributes template.

--- philo/static/philo/js/AttributeInline.js
+++ philo/static/philo/js/AttributeInline.js
@@ -5,15 +5,23 @@
 		init: function () {
 			var self = attributesInline,
 				attributesWrapper = $('#philo-attribute-entity_content_type-entity_object_id-group'),
-				valueTypeFields = $('select[name$="-value_content_type"]');
+				valueTypeFields = $('select[name$="-value_content_type"]'),
+				emptyForm = $('.empty-form', attributesWrapper),
+				jsonTemplate = $('.row:nth-child(3)'),
+				foreignKeyTemplate = $('.row:nth-child(4)'),
+				m2mTemplate = $('.row:nth-child(5)');
+			
+			// remove the fields from the template
+			jsonTemplate.detach();
+			foreignKeyTemplate.detach();
+			m2mTemplate.detach();
 				
-			valueTypeFields.bind('change.'+NS, self.valueTypeChangeHandler);
+			valueTypeFields.live('change.'+NS, self.valueTypeChangeHandler);
 		},
 		
 		valueTypeChangeHandler: function (e) {
 			var $this = $(this),
 				attributeWrapper = $this.closest('*[id^="philo-attribute-"]');
-			// TODO: actually implement this. We'll need an API for some degree of server-side interaction first.
 		}
 		
 	}
